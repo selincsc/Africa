c808606c064e05432835577a2cb323a9601d4991
Selin Çağlar <58820720+selincsc@users.noreply.github.com>
2022-12-13 17:56:27 +0300

    Initial commit

---
+++ AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Selin Çağlar on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
