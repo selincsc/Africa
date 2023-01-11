b7d4f7d3f035498b4f06bb765560635454cfc255
Selin Çağlar <58820720+selincsc@users.noreply.github.com>
2023-01-11 16:53:04 +0300

    VideoPlayer View, Model and Helpers

---
+++ Helpers/VideoPlayerHelpers.swift
@@ -0,0 +1,20 @@
+//
+//  VideoPlayerHelpers.swift
+//  Africa
+//
+//  Created by Selin Çağlar on 11.01.2023.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
+
