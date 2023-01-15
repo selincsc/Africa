ed3f71f811154d2349ca613fd455d0ee6cf707f1
Selin Çağlar <58820720+selincsc@users.noreply.github.com>
2023-01-15 20:22:16 +0300

    Grid Functions

--- App/ContentView.swift
+++ App/ContentView.swift
@@ -8,24 +8,97 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+    
+    
 //MARK: - PROPERTIES
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var isGridviewActive: Bool = false
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
+    
+    //:MARK - FUNCTIONS
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+        print("Grid Number: \(gridColumn)")
         
-        let animals: [Animal] = Bundle.main.decode("animals.json")
+        // - TOOLBAR IMAGE
+        switch gridColumn{
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+        
+    }
+    
+    
+    var body: some View {
         
 //MARK: - BODY
         NavigationView{
-            List{
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)){
-                        AnimalListView(animal: animal)
-                    }//:link
-                }//:loop
-            }//:list
+            Group{
+                if !isGridviewActive {
+                    List{
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)){
+                                AnimalListView(animal: animal)
+                            }//:link
+                        }//:loop
+                    }//list
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { animal in
+                                NavigationLink(destination: AnimalDetailView(animal: animal)){
+                                    AnimalGridItemView(animal: animal)
+                                }//link
+                            }//:loop
+                        }//:grid
+                        .padding(10)
+                        .animation(.easeIn)
+                    }//:scroll
+                }//:condition
+            }//:group
             .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16){
+                        //list
+                        Button(action: {
+                            print("List view is activated")
+                            isGridviewActive = false
+                            haptics.impactOccurred()
+                        }){
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridviewActive ? .primary : .accentColor)
+                        }
+                        //grid
+                        Button(action: {
+                            print("Grid view is acitvated")
+                            isGridviewActive = true
+                            haptics.impactOccurred()
+                            gridSwitch()
+                        }){
+                            Image(systemName: toolbarIcon)
+                                .font(.title2)
+                                .foregroundColor(isGridviewActive ? .accentColor : .primary)
+                        }
+                    }//:hstack
+                }//button
+            }//toolbar
         }//:navigation
     }
 }
